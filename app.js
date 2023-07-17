d0d416607897ec39afd623078172aafa011b9bd4
matias330 <matiasdebrasi03@gmail.com>
2023-07-18 00:30:06 +0200

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use( express.static('public') );
+
+app.listen(3000, () => {
+    console.log('Esta funcionando');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+
