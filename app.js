7faad9ff65c22cea58cdc6465080641a2796eae5
matias330 <matiasdebrasi03@gmail.com>
2023-07-18 04:36:59 +0200

    def

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv').config();
 
 app.use( express.static('public') );
 
-app.listen(3000, () => {
-    console.log('Esta funcionando');
-})
+app.listen(process.env.PORT, () => {
+    console.log('Servidor escuchando en el puerto' + process.env.PORT);
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
-})
+});
 
